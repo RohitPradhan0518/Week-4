65a8842ead4185ef9b9f3b7985a081c53c43f979
Rohitpradhan8 <rohitpradhan0518@gmail.com>
2023-10-23 15:14:26 -0400

    week 4

---
+++ simple_functions.js
@@ -0,0 +1,26 @@
+//SIMPLE FUNCTIONS EXERCISE
+
+//DEFINE THREE FUNCTION THAT *CAN* BE CALLED
+function favoriteThings(){
+    console.log("Khasi");
+    console.log("Is my favorite meat");
+}
+
+function whyImHere(){
+    console.log("Learn HTML,CSS,JAVA");
+    console.log("Learning to code");
+}
+
+function favoritePlace(){
+    console.log("Nepal");
+    console.log("Because nepal is my home town");
+}
+
+//CALL EACH IN TURN
+favoriteThings();
+whyImHere();
+favoritePlace();
+
+favoriteThings();
+whyImHere();
+favoritePlace();
